f4ab56233bc5fe5fef9dfdd9bb671e458ab9863f
David Shanske <david@shanske.com>
2014-06-05 02:15:07 -0400

    Add Location Metadata

--- kind-postmeta.php
+++ kind-postmeta.php
@@ -25,6 +25,17 @@ function kindbox_add_postmeta_boxes() {
     'normal',         // Context
     'default'         // Priority
   );
+
+  add_meta_box(
+    'locationbox-meta',      // Unique ID
+    esc_html__( 'Location', 'kind_taxonomy' ),    // Title
+    'location_metabox',   // Callback function
+    'post',         // Admin page (or post type)
+    'normal',         // Context
+    'default'         // Priority
+  );
+
+
 }
 
 function response_metabox( $object, $box ) { ?>
@@ -40,6 +51,45 @@ function response_metabox( $object, $box ) { ?>
     <br />
     <input type="text" name="response_title" id="response_title" value="<?php echo esc_attr( get_post_meta( $object->ID, 'response_title', true ) ); ?>" size="70" />
   </p>
+
+<?php }
+
+function location_metabox( $object, $box ) { ?>
+
+  <?php wp_nonce_field( 'location_metabox', 'location_metabox_nonce' ); ?>
+   <script language="javascript">
+	function getLocation()
+  	   {
+  		if (navigator.geolocation)
+			{
+		      navigator.geolocation.getCurrentPosition(showPosition);
+	   }
+  		else{alert("Geolocation is not supported by this browser.");}
+  }
+function showPosition(position)
+  {
+	document.getElementById("geo_latitude").value = position.coords.latitude;
+     	document.getElementById("geo_longitude").value = position.coords.longitude;
+
+  }
+  </script>
+
+
+  <p>
+    <label for="geo_public"><?php _e( "Public", 'kind_taxonomy' ); ?></label>
+    <input type="checkbox" name="geo_public" id="geo_public" value="<?php echo esc_attr( get_post_meta( $object->ID, 'geo_public', true ) ); ?>" />
+    <br />
+    <label for="geo_latitude"><?php _e( "Latitude", 'kind_taxonomy' ); ?></label>
+    <input type="text" name="geo_latitude" id="geo_latitude" value="<?php echo esc_attr( get_post_meta( $object->ID, 'geo_latitude', true ) ); ?>" size="30" />
+    <br />
+    <label for="geo_longitude"><?php _e( "Longitude", 'kind_taxonomy' ); ?></label>
+    <input type="text" name="geo_longitude" id="geo_longitude" value="<?php echo esc_attr( get_post_meta( $object->ID, 'geo_longitude', true ) ); ?>" size="30" />
+    <br />
+    <label for="geo_address"><?php _e( "Human-Readable Address (Optional)", 'kind_taxonomy' ); ?></label>
+    <br />
+    <input type="text" name="geo_address" id="geo_address" value="<?php echo esc_attr( get_post_meta( $object->ID, 'geo_address', true ) ); ?>" size="70" /  
+    <span class="mapp" onmouseover="getLocation()">Retrieve Location</span>
+ </p>
 
 <?php }
 
@@ -92,6 +142,62 @@ function responsebox_save_post_meta( $post_id ) {
 }
 
 add_action( 'save_post', 'responsebox_save_post_meta' );
+
+/* Save the meta box's post metadata. */
+function locationbox_save_post_meta( $post_id ) {
+
+	/*
+	 * We need to verify this came from our screen and with proper authorization,
+	 * because the save_post action can be triggered at other times.
+	 */
+
+	// Check if our nonce is set.
+	if ( ! isset( $_POST['location_metabox_nonce'] ) ) {
+		return;
+	}
+
+	// Verify that the nonce is valid.
+	if ( ! wp_verify_nonce( $_POST['location_metabox_nonce'], 'location_metabox' ) ) {
+		return;
+	}
+
+	// If this is an autosave, our form has not been submitted, so we don't want to do anything.
+	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
+		return;
+	}
+
+	// Check the user's permissions.
+	if ( isset( $_POST['post_type'] ) && 'page' == $_POST['post_type'] ) {
+
+		if ( ! current_user_can( 'edit_page', $post_id ) ) {
+			return;
+		}
+
+	} else {
+
+		if ( ! current_user_can( 'edit_post', $post_id ) ) {
+			return;
+		}
+	}
+
+	/* OK, its safe for us to save the data now. */
+	if( isset( $_POST[ 'geo_latitude' ] ) ) {
+        update_post_meta( $post_id, 'geo_latitude', esc_attr( $_POST[ 'geo_latitude' ] ) );
+	}
+	if( isset( $_POST[ 'geo_longitude' ] ) ) {
+        update_post_meta( $post_id, 'geo_longitude', esc_attr( $_POST[ 'geo_longitude' ] ) );
+    }
+	if( isset( $_POST[ 'geo_address' ] ) ) {
+        update_post_meta( $post_id, 'geo_address', esc_attr( $_POST[ 'geo_address' ] ) );
+    }
+	if( isset( $_POST[ 'geo_public' ] ) ) {
+        update_post_meta( $post_id, 'geo_public', esc_attr( $_POST[ 'geo_public' ] ) );
+    }
+
+}
+
+add_action( 'save_post', 'locationbox_save_post_meta' );
+
 
 add_action( 'add_meta_boxes', 'make_wp_editor_movable', 0 );
 	function make_wp_editor_movable() {
