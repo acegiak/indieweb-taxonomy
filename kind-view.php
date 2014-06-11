b659dfe7ea44e26192b109d36610ea1317080fa6
acegiak <acegiak@gmail.com>
2014-06-11 13:07:26 +0930

    making the content_filter work properly with functions >.<

--- kind-view.php
+++ kind-view.php
@@ -32,13 +32,13 @@ function indieweb_taxonomy_content_filter( $content ) {
 		if(in_array("response_url",get_post_custom_keys(get_the_ID()))){ 
 			$customfields = get_post_custom(get_the_ID());
 			$verbs = array();
-			$c .= '<div class="'.get_kind_class('','p').'">';
+			$c .= '<div class="'.implode(' ',get_kind_class('','p')).'">';
 			$contextbox = "";
 			if(has_kind("repost")){
 				$contextbox = '<blockquote class="p-content"><p>'.$contextbox . implode("</p><p>",$customfields["response_quote"]).'</p></blockquote>';
 			}
-			if(array_key_exists("response_title",$customfields)){
-				$contextbox = '<p>'.$contextbox.' '.the_kinds('',' and ','').' <a class="u-url" href="'.$customfields['response_url'][0].'">@'.$customfields['response_title'][0].'</a></p>';
+			if(count(get_the_kinds('','',''))>0){
+				$contextbox = '<p>'.$contextbox.' '.get_the_kinds_list('',' and ','').' <a class="u-url" href="'.$customfields['response_url'][0].'">@'.$customfields['response_title'][0].'</a></p>';
 			}
 
 		$c .= $contextbox.'</div>'; }
