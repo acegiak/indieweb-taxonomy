d888d4d3d66aa730366ff1ec8d7bc376c4442558
David Shanske <david@shanske.com>
2014-06-13 15:34:25 -0400

    Forked the Location Component into a Separate Plugin

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ indieweb-taxonomy
 
 Indieweb Taxonomy adds a Custom Taxonomy to the standard post type in Wordpress that allows posts to have a semantic componenent. This allows archives of replies, likes, reposts, etc. as well as theme support to add the appropriate classes to links within same.
 
+Version 0.03 - Location function removed and split into separate plugin called Simple Location. Any check-in kind functionality will have this as a dependency
+
 Version 0.02 - Location meta box with HTML5 geolocation fill-in added. This allows posts to optionally have a location. This is as per the Wordpress Geodata specifications, so the Wordpress Android app will fill them in. There is no display functionality. Various functions that mimic the built-in functions for other taxonomies were added, including filters to add additional behaviors. Default terms now prepopulate if no terms exist.
 
 Version 0.01 - Registers a custom taxonomy, adds in code snippets to turn the post meta box from checkboxes to radio buttons, adds code to allow a custom permalink tag if needed.
--- kind-postmeta.php
+++ kind-postmeta.php
@@ -26,16 +26,6 @@ function kindbox_add_postmeta_boxes() {
     'default'         // Priority
   );
 
-  add_meta_box(
-    'locationbox-meta',      // Unique ID
-    esc_html__( 'Location', 'kind_taxonomy' ),    // Title
-    'location_metabox',   // Callback function
-    'post',         // Admin page (or post type)
-    'normal',         // Context
-    'default'         // Priority
-  );
-
-
 }
 
 function response_metabox( $object, $box ) { ?>
@@ -58,46 +48,6 @@ function response_metabox( $object, $box ) { ?>
   </p>
 
 <?php }
-
-function location_metabox( $object, $box ) { ?>
-
-  <?php wp_nonce_field( 'location_metabox', 'location_metabox_nonce' ); ?>
-   <script language="javascript">
-	function getLocation()
-  	   {
-  		if (navigator.geolocation)
-			{
-		      navigator.geolocation.getCurrentPosition(showPosition);
-	   }
-  		else{alert("Geolocation is not supported by this browser.");}
-  }
-function showPosition(position)
-  {
-	document.getElementById("geo_latitude").value = position.coords.latitude;
-     	document.getElementById("geo_longitude").value = position.coords.longitude;
-
-  }
-  </script>
-
-
-  <p>
-    <label for="geo_public"><?php _e( "Public", 'kind_taxonomy' ); ?></label>
-    <input type="checkbox" name="geo_public" id="geo_public" value="1" />
-    <br />
-    <label for="geo_latitude"><?php _e( "Latitude", 'kind_taxonomy' ); ?></label>
-    <input type="text" name="geo_latitude" id="geo_latitude" value="<?php echo esc_attr( get_post_meta( $object->ID, 'geo_latitude', true ) ); ?>" size="30" />
-    <br />
-    <label for="geo_longitude"><?php _e( "Longitude", 'kind_taxonomy' ); ?></label>
-    <input type="text" name="geo_longitude" id="geo_longitude" value="<?php echo esc_attr( get_post_meta( $object->ID, 'geo_longitude', true ) ); ?>" size="30" />
-    <br />
-    <label for="geo_address"><?php _e( "Human-Readable Address (Optional)", 'kind_taxonomy' ); ?></label>
-    <br />
-    <input type="text" name="geo_address" id="geo_address" value="<?php echo esc_attr( get_post_meta( $object->ID, 'geo_address', true ) ); ?>" size="70" />   
-     <button type="button" onclick="getLocation();return false;">Retrieve Location</button>
- </p>
-
-<?php }
-
 
 /* Save the meta box's post metadata. */
 function responsebox_save_post_meta( $post_id ) {
@@ -150,62 +100,6 @@ function responsebox_save_post_meta( $post_id ) {
 }
 
 add_action( 'save_post', 'responsebox_save_post_meta' );
-
-/* Save the meta box's post metadata. */
-function locationbox_save_post_meta( $post_id ) {
-
-	/*
-	 * We need to verify this came from our screen and with proper authorization,
-	 * because the save_post action can be triggered at other times.
-	 */
-
-	// Check if our nonce is set.
-	if ( ! isset( $_POST['location_metabox_nonce'] ) ) {
-		return;
-	}
-
-	// Verify that the nonce is valid.
-	if ( ! wp_verify_nonce( $_POST['location_metabox_nonce'], 'location_metabox' ) ) {
-		return;
-	}
-
-	// If this is an autosave, our form has not been submitted, so we don't want to do anything.
-	if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE ) {
-		return;
-	}
-
-	// Check the user's permissions.
-	if ( isset( $_POST['post_type'] ) && 'page' == $_POST['post_type'] ) {
-
-		if ( ! current_user_can( 'edit_page', $post_id ) ) {
-			return;
-		}
-
-	} else {
-
-		if ( ! current_user_can( 'edit_post', $post_id ) ) {
-			return;
-		}
-	}
-
-	/* OK, its safe for us to save the data now. */
-	if( isset( $_POST[ 'geo_latitude' ] ) ) {
-        update_post_meta( $post_id, 'geo_latitude', esc_attr( $_POST[ 'geo_latitude' ] ) );
-	}
-	if( isset( $_POST[ 'geo_longitude' ] ) ) {
-        update_post_meta( $post_id, 'geo_longitude', esc_attr( $_POST[ 'geo_longitude' ] ) );
-    }
-	if( isset( $_POST[ 'geo_address' ] ) ) {
-        update_post_meta( $post_id, 'geo_address', esc_attr( $_POST[ 'geo_address' ] ) );
-    }
-	if( isset( $_POST[ 'geo_public' ] ) ) {
-        update_post_meta( $post_id, 'geo_public', esc_attr( $_POST[ 'geo_public' ] ) );
-    }
-
-}
-
-add_action( 'save_post', 'locationbox_save_post_meta' );
-
 
 add_action( 'add_meta_boxes', 'make_wp_editor_movable', 0 );
 function make_wp_editor_movable() {
